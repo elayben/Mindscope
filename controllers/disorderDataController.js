751658440ed6a39a14a157f5faa123299d9a45c0
OrHatzav2 <orhatzav@gmail.com>
2025-01-08 16:46:28 +0200

    all-countries

--- controllers/disorderDataController.js
+++ controllers/disorderDataController.js
@@ -264,3 +264,25 @@ exports.getDisorderCorrelation = async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch disorder correlation' });
   }
 };
+
+// 13) Get all countries
+exports.getAllCountriesWithCodes = async (req, res) => {
+  try {
+    const [rows] = await db.execute(
+      `SELECT country_name, country_code FROM countries ORDER BY country_name ASC`
+    );
+
+    const countriesWithCodes = rows.reduce((acc, row) => {
+      acc[row.country_name] = row.country_code;
+      return acc;
+    }, {});
+
+    res.json(countriesWithCodes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch countries and their codes' });
+  }
+};
+
+
+
